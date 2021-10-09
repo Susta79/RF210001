5d51a8b09f85ec0a75dab67516f33e04e7ae8a18
Fabrizio Bergamin <fabriberga@gmail.com>
2021-10-09 07:09:38 +0200

    gitignore update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/fabri/Nextcloud/Project/IR01_IndustrialRobotics/RF210001
-# Build directory: /Users/fabri/Nextcloud/Project/IR01_IndustrialRobotics/RF210001/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
